261719dc78bb5362c82e36249e3ff8b541504635
Johannes Riecken <johannes.riecken@gmail.com>
2024-05-20 09:07:52 +0200

    Translate MaxData.java to Haskell

---
+++ MaxData.hs
@@ -0,0 +1,24 @@
+import Data.Map (Map(..))
+
+data MaxData = MaxData { mdMaterials :: [Material], mdNodes :: Map String Node, mdRoots :: Map String Node }
+
+data MappingChannel = MappingChannel { mcMtPoints :: Int, mcTPoints :: [Float], mcFaces :: [Int] }
+
+data Mesh = Mesh { mesName :: String, mesNPoints :: Int, mesPoints :: [Float], mesNFaces :: Int, mesFaces :: [Int], mesMapping :: [MappingChannel] }
+
+data NodeTM = NodeTM { ntmName :: String, ntmPos :: Point3D, ntmTm :: [Point3D] }
+
+data Point3D = Point3D Int Int Int
+
+data Material = Material { matName :: String, matDiffuseMap :: String, mapAmbientColor :: Point3D, mapDiffuseColor :: Point3D, mapSpecularColor :: Point3D }
+
+data Node = Node { nodName :: String, nodeNodeTM :: NodeTM, nodParent :: Node, nodChildren :: [Node] }
+
+data LightNode = LightNode { lnIntensity :: Float, lnR :: Float, lnG :: Float, lnB :: Float, {- inherited -} lnNode :: Node }
+
+data CameraNode = CameraNode { cnTarget :: NodeTM, cnNear :: Float, cnFar :: Float, cnFov :: Float, {- inherited -} cnNode :: Node }
+
+data GeomNode = GeomNode { gnMesh :: Mesh, gnMaterialRef :: Int, {- inherited -} gnNode :: Node }
+
+main :: IO ()
+main = pure ()
